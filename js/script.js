36ed87940ceab7eb9b6d1fff2f1ef71116685213
laurachebet1997 <rosechebet19971997@gmail.com>
2021-01-05 17:52:48 +0300

    add response to button on click

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $("#design-text").click(function() {
+        $("#myDIV").toggle();
+    });
+}); 
+$(document).ready(function() {
+    $("#details3").click(function() {
+        $("#product-text").toggle();
+    });
+}); 
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+} 
+function myData() {
+    var x = document.getElementById("myDIv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myText() {
+    var x = document.getElementById("myDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+} 
+function submit() {
+    alert("contact successfully sent")
+}   
